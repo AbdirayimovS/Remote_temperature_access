202458c2994ed08aecbd7fab977a6d3233992969
Sardor <96771858+AbdirayimovS@users.noreply.github.com>
2023-01-21 09:02:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,31 +6,31 @@ import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
 
-# firebaseConfig = {
-#   'apiKey': st.secrets["apikey"],
-#   'authDomain': st.secrets["authdomain"],
-#   'databaseURL': "https://remote-temperature-20726-default-rtdb.firebaseio.com",
-#   'projectId': "remote-temperature-20726",
-#   'storageBucket': "remote-temperature-20726.appspot.com",
-#   'messagingSenderId': st.secrets["messagingsenderid"],
-#   'appId': st.secrets["appid"],
-#   'measurementId': st.secrets["measurementid"]
-# }
+firebaseConfig = {
+  'apiKey': st.secrets["apikey"],
+  'authDomain': "remote-temperature-20726.firebaseapp.com",
+  'databaseURL': "https://remote-temperature-20726-default-rtdb.firebaseio.com",
+  'projectId': "remote-temperature-20726",
+  'storageBucket': "remote-temperature-20726.appspot.com",
+  'messagingSenderId': "221631060825",
+  'appId': "1:221631060825:web:9c416c5a2b2e7c0b75ca48",
+  'measurementId':"G-MWMF1EPKBB"
+}
 
-# firebase = pyrebase.initialize_app(firebaseConfig)
+firebase = pyrebase.initialize_app(firebaseConfig)
 
-# db = firebase.database()
-# users = db.child("DHT").get()
-# df = pd.DataFrame(users.val()).T
-# df.index = df.index.astype(int)
-# main_df = df[df.index > 1600000000]
-#  #(30968, 2)
+db = firebase.database()
+users = db.child("DHT").get()
+df = pd.DataFrame(users.val()).T
+df.index = df.index.astype(int)
+main_df = df[df.index > 1600000000]
+ #(30968, 2)
 
-# last_update = main_df.tail(1).index[0]
-# # st.header(last_update)
-# # print(last_update)
+last_update = main_df.tail(1).index[0]
+# st.header(last_update)
+# print(last_update)
 
-# dttime = datetime.utcfromtimestamp(last_update).strftime('%Y-%m-%d %H:%M:%S')
+dttime = datetime.utcfromtimestamp(last_update).strftime('%Y-%m-%d %H:%M:%S')
 
 #-----------------------------------------------------#
 
